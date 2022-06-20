3d2a7f8bac162f977bf4d57bcaa33c0b4159da01
Konrad Milewski <konradmilewski2000@interia.eu>
2022-06-20 22:36:10 +0200

    Base non-login routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "../src/sass/App.scss";
+import Welcome from "./components/home/Welcome";
 
 function App() {
-  return <div className="App"></div>;
+  const isUserLogged = false;
+
+  return (
+    <Routes>
+      {!isUserLogged && (
+        <Route path="/">
+          <Route path="login"></Route>
+          <Route path="login"></Route>
+          <Route index element={<Welcome></Welcome>}></Route>
+          <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+        </Route>
+      )}
+      {isUserLogged && <Route></Route>}
+    </Routes>
+  );
 }
 
 export default App;
